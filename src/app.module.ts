03b53e4331eb48d198f3aa834e31181eeceb9625
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-08-31 21:47:19 -0300

    chore(auth): create module, controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PermissionsModule } from './permissions/permissions.module';
 import { UsersModule } from './users/users.module';
 import { PermissionsLevelsModule } from './permissions-levels/permissions-levels.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PermissionsModule, UsersModule, PermissionsLevelsModule],
+  imports: [PermissionsModule, UsersModule, PermissionsLevelsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
